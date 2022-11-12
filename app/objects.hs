f00588723ff0b324e8cce8fb61298d93cf93dc8d
STEFAN <sherald323@gmail.com>
2022-11-12 18:43:41 +0100

    oops missed shootDir

--- app/objects.hs
+++ app/objects.hs
@@ -1,14 +1,13 @@
 module Objects where
 type Dir      = (Float, Float)
 type Pos      = (Float, Float)
-type ShootDir = (Float, Float)
 type Health   = Int
-data Obstacle = Enemy Health Pos Dir ShootDir
+data Obstacle = Enemy Health Pos Dir
                 | Asteroid Health Pos Dir
                 | Mine Health Pos
                 | Projectile Pos Dir
                 deriving (Show)
-data Player   = Player Health Pos Dir ShootDir
+data Player   = Player Health Pos Dir
                 deriving (Show)
 initialPlayer :: Player
 initialPlayer = Player 3 (300, 300) (0,0) (0,0)
